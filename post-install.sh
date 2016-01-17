0874f10349c5220fca7e64bf22da45e2e83718f7
trincoingels <trincoingels@gmail.com>
2016-01-17 17:19:09 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,2 +1,2 @@
-printf "\nwfLoadSkin( 'deltaskin' );" >> ../../../LocalSettings.php
-printf "\n\$wgDefaultSkin = \"deltaskin\";" >> ../../../LocalSettings.php
+printf "\nwfLoadSkin( 'deltaskin' );" >> ../../LocalSettings.php
+printf "\n\$wgDefaultSkin = \"deltaskin\";" >> ../../LocalSettings.php
